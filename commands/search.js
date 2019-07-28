f4c3dfbd7659c2522da41440f5765e89669210ff
ProPanek <oga.seb@gmail.com>
2019-07-28 22:06:16 +0200

    debugging

--- commands/search.js
+++ commands/search.js
@@ -1,5 +1,5 @@
 import { findGame } from "../db/db_qree";
-import { createEmbeddedAnswer } from "../helpers/helpers";
+import { checkIfDM, createEmbeddedAnswer } from "../helpers/helpers";
 import pgEscape from "pg-escape";
 
 export async function searchGame(messageArguments, receivedMessage) {
@@ -16,12 +16,21 @@ export async function searchGame(messageArguments, receivedMessage) {
       .replace(/'/g, "''");
     const { rows } = await findGame(name);
     if (rows.length === 0) {
-      return await receivedMessage.channel.send(
-        `I didn't find anything called \`${messageArguments[1].replace(
-          /^"(.*)"$/,
-          "$1"
-        )}\` in my database. Please check <#582266411166990346> for it, if it's there please inform Uploaders/Mods/Bot Dev about adding it to the bot. If it's not there either request game on <#582262747937505290>`
-      );
+      if (checkIfDM(receivedMessage)) {
+        return await receivedMessage.channel.send(
+          `I didn't find anything called \`${messageArguments[1].replace(
+            /^"(.*)"$/,
+            "$1"
+          )}\` in my database. If you want to request games join https://discord.gg/uJnP5q`
+        );
+      } else {
+        return await receivedMessage.channel.send(
+          `I didn't find anything called \`${messageArguments[1].replace(
+            /^"(.*)"$/,
+            "$1"
+          )}\` in my database. Please check <#582266411166990346> for it, if it's there please inform Uploaders/Mods/Bot Dev about adding it to the bot. If it's not there either request game on <#582262747937505290>`
+        );
+      }
     } else {
       const QrCodesSearchResults = await createEmbeddedAnswer(
         rows,
