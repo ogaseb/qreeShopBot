f131b5a52a4005a02578a3cab7f9505028de2919
ProPanek <oga.seb@gmail.com>
2019-07-30 21:02:18 +0200

    script for checking urls update

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -157,24 +157,23 @@ export async function urlStatus(client) {
     .get("604692367018033152")
     .send(`Checking urls started... I will do it every 24 hours`);
   const { rows } = await getWholeDB();
-  for (const { id, qr_link, name } of rows) {
+  for (const { id, qr_link, name, uploader_discord_id } of rows) {
     try {
       console.log(`starting scanning ${name}`);
-      const response = await axios.head(qr_link, { timeout: 15000 });
     } catch (e) {
       if (e.response) {
         if (e.response.status === 404) {
           await client.channels
             .get("604692367018033152")
             .send(
-              `${qr_link} sends ${e.response.status} respond code (not found or other error) for game: ${name}. DB ID for updating: ${id} .`
+              `${qr_link} sends ${e.response.status} respond code (not found or other error) for game: ${name}. DB ID for updating: ${id} . <@${uploader_discord_id}>`
             );
         }
       } else {
         await client.channels
           .get("604692367018033152")
           .send(
-            `${qr_link} sends error respond code (not found or other error) for game: ${name}. DB ID for updating: ${id} .`
+            `${qr_link} sends error respond code (not found or other error) for game: ${name}. DB ID for updating: ${id} . <@${uploader_discord_id}>`
           );
       }
     }
